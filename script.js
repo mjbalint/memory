9164b62ad8f1cd82143380ef166ccca675057544
Matthew Balint <mjbalint@gmail.com>
2014-12-30 14:27:52 -0800

    Added flags for use with memory circles

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var clickCount = 0;
+var $lastPiece = null;
 
 // Initialize web page 
 $(document).ready(function(){
-    $('#update_me').html('<p>It is I, the ready function!</p>');
-
     $('.piece').click(function() {
-        clickCount++;
-        $('#update_me').html('<p>Click #' + clickCount + '</p>');
-        $(this).addClass('selected');
+        if (null !== $lastPiece) {
+            $lastPiece.removeClass('selected');
+        }
+        $lastPiece = $(this);
+        $lastPiece.addClass('selected');
     });
 });
